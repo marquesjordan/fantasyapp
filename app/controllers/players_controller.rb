8c9827e99589bd5dff11fbaf68a4d9ddfc59e8da
Marques Jordan <marques.jordan81@gmail.com>
2014-12-23 23:23:19 -0800

    Players Conroller adjusted for api call

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,64 +7,64 @@ class PlayersController < ApplicationController
     players = Player.all
     # render json: players, status: 200
 
-    # p = Player.all
-    # p.each do |player|
+    p = Player.all
+    p.each do |player|
       
-    #   number = player.fantasy * 238
-    #   player.cost = (number.floor/100)*100
-    #   if player.cost < 3500
-    #     player.cost = 3500
-    #   end
-    #   player.save
-    # end
+      number = player.fantasy * 238
+      player.cost = (number.floor/100)*100
+      if player.cost < 3500
+        player.cost = 3500
+      end
+      player.save
+    end
     
     ################# API PULL CODE #############################
     
     # @puri = HTTParty.get"http://api.sportsdatallc.org/nba-t3/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/profile.xml?api_key=3jdhu8c22zst6mn3p6fajnze"
     # @teams.each do |t|
-    teams = Team.all
-    @count = teams.length
+    # teams = Team.all
+    # @count = teams.length
     
     ####################### CALLING API FOR TEAM STATS & PLAYER INFO ########################
 
-  for i in 0...@count
-    tid = teams[i].nba_team_id
+  # for i in 0...@count
+  #   tid = teams[i].nba_team_id
 
-    # @puri = HTTParty.get"http://api.sportsdatallc.org/nba-t3/seasontd/2014/REG/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/statistics.xml?api_key=3jdhu8c22zst6mn3p6fajnze"
+  #   # @puri = HTTParty.get"http://api.sportsdatallc.org/nba-t3/seasontd/2014/REG/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/statistics.xml?api_key=3jdhu8c22zst6mn3p6fajnze"
 
-      @url = "http://api.sportsdatallc.org/nba-t3/seasontd/2014/REG/teams/#{tid}/statistics.xml?api_key=3jdhu8c22zst6mn3p6fajnze"
+  #     @url = "http://api.sportsdatallc.org/nba-t3/seasontd/2014/REG/teams/#{tid}/statistics.xml?api_key=3jdhu8c22zst6mn3p6fajnze"
 
-      # @response_array = []
+  #     # @response_array = []
       
-      begin   # raisebegin
-         @puri = HTTParty.get(@url)
-         team_count = @puri['season']['team']['player_records']['player'].count
-      rescue
-        sleep 1
-        @puri = HTTParty.get(@url)
-      ensure 
+  #     begin   # raisebegin
+  #        @puri = HTTParty.get(@url)
+  #        team_count = @puri['season']['team']['player_records']['player'].count
+  #     rescue
+  #       sleep 1
+  #       @puri = HTTParty.get(@url)
+  #     ensure 
        
-      end
+  #     end
 
-      team_count = @puri['season']['team']['player_records']['player'].count
-      for x in 0...team_count
+  #     team_count = @puri['season']['team']['player_records']['player'].count
+  #     for x in 0...team_count
       
 
-        pid = @puri['season']['team']['player_records']['player'][x]['id']
-        name = @puri['season']['team']['player_records']['player'][x]['full_name']
-        pos = @puri['season']['team']['player_records']['player'][x]['primary_position']
-        pts = @puri['season']['team']['player_records']['player'][x]['overall']['average']['points'].to_f
-        ast = @puri['season']['team']['player_records']['player'][x]['overall']['average']['assists'].to_f
-        reb = @puri['season']['team']['player_records']['player'][x]['overall']['average']['rebounds'].to_f
-        stl = @puri['season']['team']['player_records']['player'][x]['overall']['average']['steals'].to_f
-        trn = @puri['season']['team']['player_records']['player'][x]['overall']['average']['turnovers'].to_f
-        blk = @puri['season']['team']['player_records']['player'][x]['overall']['average']['blocks'].to_f
+  #       pid = @puri['season']['team']['player_records']['player'][x]['id']
+  #       name = @puri['season']['team']['player_records']['player'][x]['full_name']
+  #       pos = @puri['season']['team']['player_records']['player'][x]['primary_position']
+  #       pts = @puri['season']['team']['player_records']['player'][x]['overall']['average']['points'].to_f
+  #       ast = @puri['season']['team']['player_records']['player'][x]['overall']['average']['assists'].to_f
+  #       reb = @puri['season']['team']['player_records']['player'][x]['overall']['average']['rebounds'].to_f
+  #       stl = @puri['season']['team']['player_records']['player'][x]['overall']['average']['steals'].to_f
+  #       trn = @puri['season']['team']['player_records']['player'][x]['overall']['average']['turnovers'].to_f
+  #       blk = @puri['season']['team']['player_records']['player'][x]['overall']['average']['blocks'].to_f
 
-        fan = ( (pts * 1 ) + (ast * 1.5) + (reb * 1.2 ) + (stl * 2) + (blk * 2) ) - (trn * 1)
-        fan = fan.round(2)
-        Player.create(player_id: pid, team_id: tid, name: name, position: pos, points: pts, assists: ast, rebounds: reb, steals: stl, turnovers: trn, blocks: blk, fantasy: fan,cost: 0)
-      end
-    end
+  #       fan = ( (pts * 1 ) + (ast * 1.5) + (reb * 1.2 ) + (stl * 2) + (blk * 2) ) - (trn * 1)
+  #       fan = fan.round(2)
+  #       Player.create(player_id: pid, team_id: tid, name: name, position: pos, points: pts, assists: ast, rebounds: reb, steals: stl, turnovers: trn, blocks: blk, fantasy: fan,cost: 0)
+  #     end
+  #   end
 
 
 
