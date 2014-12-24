1a159352c6859f23013666476cc9043a21bee613
Marques Jordan <marques.jordan81@gmail.com>
2014-12-23 20:45:56 -0800

    Place Angular.min.js in views/layouts/application

--- app/assets/javascripts/ng_app/app.js
+++ app/assets/javascripts/ng_app/app.js
@@ -261,7 +261,7 @@ $urlRouterProvider.otherwise('/about');
   });
 
 }])
-.service('api', ['$http', '$location',function($http, $location) {
+.service('api', ['$http', '$location', function($http, $location) {
      return {
           getPlayers: function() {
 
